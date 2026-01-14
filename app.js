15ea56b48735457394441829a0749436966f29cb
y0749278-cloud <y0749278@gmail.com>
2026-01-14 14:59:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -141,6 +141,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => { console.log('Private Chat Online'); });
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => { console.log('Server is online'); });
+
 
